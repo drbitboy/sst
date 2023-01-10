5c337a7ce881be823e15a22faa9b88e7d4ca60ad
Brian T. Carcich <BrianTCarcich@gmail.com>
2023-01-10 15:40:51 -0700

    improve logging

--- spy.py
+++ spy.py
@@ -14,17 +14,23 @@ opts = dict(baudrate=12500000
 def reader():
     ser = serial.Serial(**opts)
     ct = 0
+    total_count = 0
     for buf in ser.iread_until():
-        if not ct: print((buf,))
-        if ct > 940093: print(ct) ; ct -= 940094
-    print(dict(close=ser.close()))
+        total_count += len(buf)
+        if not ct:
+            print((buf,))
+        elif ct > 94093: print(ct) ; ct -= 94094
+        ct += 1
+    opts.update(dict(close=ser.close(),total_count=total_count))
+    print(opts)
 
 def writer():
     ser = serial.Serial(**opts)
     buf = bytes(list(range(33,127))+[10])
     mL = -len(buf) - 1
-    total_count = opts['baudrate']
-    log_count = total_count - 500000
+    baudrate = total_count = opts['baudrate']
+    dlog_count = total_count // 25
+    log_count = total_count - dlog_count
     while total_count > 0:
         for mi in range(-1,mL,-1):
             incr_count = ser.write(buf[mi:])
@@ -32,8 +38,9 @@ def writer():
                 total_count -= incr_count
                 if total_count <= log_count:
                     print(total_count)
-                    log_count -= 500000
-    print(dict(close=ser.close()))
+                    log_count -= dlog_count
+    opts.update(dict(close=ser.close(),total_count=baudrate-total_count))
+    print(opts)
 
 if "__main__" ==__name__ and sys.argv[1:]:
 
